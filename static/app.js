4c8dc99f4e10182f1f8f50d7cd5195173fdd51c4
sumudu <sumudumf@gmail.com>
2024-08-01 11:06:32 +0530

    added client application with static file serving

---
+++ static/app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const data = [
+    {id: 'A1', name: 'Vacuum Cleaner', rrp: 100.00, info: 'The suckiest vacuum in the world.'},
+    {id: 'A2', name: 'Leaf Blower', rrp: 250.00, info: 'This product will blow your socks off.'},
+    {id: 'B1', name: 'Chocolate bar', rrp: 20.00, info: 'Delicious overpriced chocolate.'}
+];
+
+const populateProducts = async () => {
+    /** clear any content in the products div element */
+    const products = document.querySelector('#products');
+    products.innerHTML = '';
+
+    for(const product of data) {
+        const item = document.createElement('product-item');
+        for(const key of ['name', 'rrp', 'info']) {
+            const span = document.createElement('span');
+            span.slot = key;
+            span.textContent = product[key];
+            item.appendChild(span);
+        }
+        products.appendChild(item);
+    }
+}
+
+document.querySelector('#fetch').addEventListener('click', async () => {
+    await populateProducts();
+});
+
+class Item extends HTMLElement {
+    constructor() {
+        super();
+        const itemTemplate = document.querySelector('#item').content;
+        const shadow = this.attachShadow({mode: 'open'}); // mode: 'open' -> allows access to the underlying shadowRoot
+        shadow.appendChild(itemTemplate.cloneNode(true)); // cloneNode(true) -> includes the subtree
+    }
+}
+
+customElements.define('product-item', Item);
