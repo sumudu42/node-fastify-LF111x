fd49a22d2f8345bab03e6ed515bb33276f249a98
sumudu <sumudumf@gmail.com>
2024-08-01 11:20:08 +0530

    added mock service with node.js

--- static/app.js
+++ static/app.js
@@ -1,15 +1,19 @@
-'use strict'
+const API = 'http://localhost:3000';
 
-const data = [
-    {id: 'A1', name: 'Vacuum Cleaner', rrp: 100.00, info: 'The suckiest vacuum in the world.'},
-    {id: 'A2', name: 'Leaf Blower', rrp: 250.00, info: 'This product will blow your socks off.'},
-    {id: 'B1', name: 'Chocolate bar', rrp: 20.00, info: 'Delicious overpriced chocolate.'}
-];
+// const data = [
+//     {id: 'A1', name: 'Vacuum Cleaner', rrp: 100.00, info: 'The suckiest vacuum in the world.'},
+//     {id: 'A2', name: 'Leaf Blower', rrp: 250.00, info: 'This product will blow your socks off.'},
+//     {id: 'B1', name: 'Chocolate bar', rrp: 20.00, info: 'Delicious overpriced chocolate.'}
+// ];
 
 const populateProducts = async () => {
     /** clear any content in the products div element */
     const products = document.querySelector('#products');
     products.innerHTML = '';
+
+    /** fetch data from the mock server */
+    const res = await fetch(API);
+    const data = await res.json();
 
     for(const product of data) {
         const item = document.createElement('product-item');
