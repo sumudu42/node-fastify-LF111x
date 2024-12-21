8f576369708999ef38163f08d5f9fd75a540700a
s_dev <sumudumf@gmail.com>
2024-12-21 06:55:15 +0530

    modified populateProducts() function to add items, added arguments method and payload to the populateProducts method and modified logic to make post requests with payload

--- static/app.js
+++ static/app.js
@@ -4,13 +4,19 @@ const API = 'http://localhost:3000';
 
 const keys = ['name', 'rrp', 'info'];
 
-const populateProducts = async (category) => {
+const populateProducts = async (category, method = "GET", payload) => {
     // clear any content in the #products div
     const products = document.querySelector('#products');
     products.innerHTML = '';
 
+    // build the response body, null in case of get request
+    const send = method === "GET" ? {} : {  
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+    }
+
     // fetch data from the remote server
-    const res = await fetch(`${API}/${category}`);
+    const res = await fetch(`${API}/${category}`, {method, ...send});
     const data = await res.json();
 
     for (const product of data) {
@@ -25,10 +31,24 @@ const populateProducts = async (category) => {
     }
 }
 
+const add = document.querySelector('#add');
 const category = document.querySelector('#category');
+
 category.addEventListener('input', async ({ target }) => {
-    console.log('target: ', target, ' value: ', target.value);
+    add.style.display = 'block';
     await populateProducts(target.value);
+});
+
+add.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const { target } = e;
+    const payload = {
+        name: target.name.value,
+        rrp: target.rrp.value,
+        info: target.info.value
+    }
+    await populateProducts(category.value, 'POST', payload);
+    target.reset();
 });
 
 customElements.define('product-item', class Item extends HTMLElement {
